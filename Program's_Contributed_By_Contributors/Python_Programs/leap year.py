697cd5866eff2e769724b4f1323358738929a022
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-10-02 23:15:32 +0530

    Update leap year.py
    Used If else condition to check leap year

--- Program's_Contributed_By_Contributors/Python_Programs/leap year.py
+++ Program's_Contributed_By_Contributors/Python_Programs/leap year.py
@@ -1,16 +1,8 @@
 # Check leap year
-
+#Leap year is sequence of every 4 years so it should be only divisble by 4 or 400 but not divisble by 100 to check if its a leap year or not.
 def leap_year_check(yr):
-    leap = False
-    if yr % 4 == 0:
-        leap = True
-        if yr % 100 ==0 and yr % 400 != 0:
-            leap = False
-        else: 
-            leap = True
-    if leap: 
-        print("{0} is a leap year".format(yr))
-    else: 
-        print("{0} is not a leap year".format(yr))
-
+    if (yr % 4 == 0 and yr % 100 != 0) or (yr % 400 == 0) == 0:
+        print("Its a leap year")
+    else:
+        print("Its not a leap year")
 leap_year_check(1700)
